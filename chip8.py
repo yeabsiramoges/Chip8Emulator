bf9f156ad1aea7d234b238a859bfd4f5ea4a4cdd
yeabsiramoges <yeab@mit.edu>
2022-01-20 12:52:50 -0500

    added output to 64x32 display

--- chip8.py
+++ chip8.py
@@ -1,2 +1,5 @@
-#inputs
+#input
 self.key_inputs = [0]*16 #storing key input states from a 16 button keyboard
+
+#output
+self.display_buffer = [0]*32*64 #outputs to a 64x32 display, an array of pixels
